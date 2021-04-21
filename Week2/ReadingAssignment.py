424af48a27ccbf256e5eb1717071519d9ece7397
Arthur Mustafin <arthurmustafin@Local587451.local>
2021-04-21 15:15:41 -0700

    week2

---
+++ Week2/ReadingAssignment.py
@@ -0,0 +1,18 @@
+print("Example one:")
+print(3+7)
+print(type(3+7))
+print(2-1)
+print("this is a chunk of text")
+print(type("this is a chunk of text"))
+
+print("Example two:")
+a = 3 + 5
+b = a * a - a - 1
+c = a * b
+print(c)
+
+print("Example three:")
+a = -6                      # previous value erased
+b = a * a - a - 1
+c = a * b
+print(c)
