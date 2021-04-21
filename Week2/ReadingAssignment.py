5558a70971ab1565c6f713a61066f55dfaaa8826
Arthur Mustafin <arthurmustafin@Local587451.local>
2021-04-21 16:32:15 -0700

    week2

--- Week2/ReadingAssignment.py
+++ Week2/ReadingAssignment.py
@@ -1,3 +1,5 @@
+# source https://www.youtube.com/watch?v=husPzLE6sZc&t
+print("LESSON 1: INTRODUCTION TO PROGRAMS DATA TYPES AND VARIABLE")
 print("Example one:")
 print(3+7)
 print(type(3+7))
@@ -16,3 +18,63 @@ a = -6                      # previous value erased
 b = a * a - a - 1
 c = a * b
 print(c)
+
+print("Example four:")
+a = -6
+b = a * a - a - 1
+c = a * b
+if a < 0:
+    print(c)
+else:
+    print(c-a)
+
+print("Example five:")
+a = 9
+b = a * a - a - 1
+c = a * b
+if a < 0:
+    print("a<0")
+    print(c)
+else:
+    print("a is not less than 0")
+    print(c-a)
+print("we are done with the program")
+
+# source https://www.youtube.com/watch?v=iZAtkS0F-Zo
+print("LESSON 2: FUN WITH STRINGS")
+print("Example one:")
+a = "My first test string"
+b = 'Another test string that I have defined'
+c = "This is sal's string"
+d = 'My favourite word is "asparaus", what is yours?'
+math_string = "3+4*2"
+expression_string = "a+' '+b+' tiger'"
+
+print(len(math_string))
+print(len(a))
+a_with_b = a + " " + b
+b_with_a = b + a
+print(a_with_b)
+print(b_with_a)
+new_string = math_string + expression_string
+print(new_string)
+
+e = b.split(' ')
+f = b.split('t')
+g = math_string.find('*')
+h = math_string.find('3')
+c.replace('i', 'o')
+c = c.replace('i', 'o')
+j = eval(math_string)           # run an operation inside the string
+k = eval(math_string + '1')     # 3+4*21
+l = eval(expression_string)     #
+print(e, f, g, h, c, j, k, l)
+
+
+
+
+
+
+
+
+
